c615460e6fd08460e6280405ac36d88f2a3dab97
Trung Kien Pham <kien-macbook@Trungs-MacBook-Air.local>
2026-08-25 23:54:38 +0700

    update PINNs

--- fPINNs/pendulum-1.py
+++ fPINNs/pendulum-1.py
@@ -6,7 +6,7 @@ import matplotlib.animation as animation
 g = 10
 l = 1
 beta = 0.0  # Damping coefficient
-theta0 = np.pi*0.1  # Initial angle
+theta0 = np.pi*0.999  # Initial angle
 
 N = 5000
 t = np.linspace(0, 50, N)  # Time array
